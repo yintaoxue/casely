9a4ba14e90e3e700147def19ae051efdb30908fe
talin <yintaoxue@gmail.com>
2014-02-28 22:41:28 +0800

    init import netty usecase

---
+++ netty-uc/src/main/java/org/ruogu/netty/handler/DiscardServerHandler.java
@@ -0,0 +1,20 @@
+package org.ruogu.netty.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class DiscardServerHandler extends ChannelHandlerAdapter {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		((ByteBuf)msg).release();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+	
+}
